a8f24d5a84606c0c1392dc605d33f772a8b51260
blockparole <48992448+blockparole@users.noreply.github.com>
2020-07-13 20:35:08 +0200

    skeleton project

---
+++ src/main/java/not/hub/mcdib/Mod.java
@@ -0,0 +1,17 @@
+package not.hub.mcdib;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Mod extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
